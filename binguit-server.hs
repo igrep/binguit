de5ba1a8ba045b1b5c672f810af5dddf2c20f74b
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2013-01-19 23:55:14 +0900

    added a conduit to choose input to the server. but not work.
    I should've added it to the client

--- binguit-server.hs
+++ binguit-server.hs
@@ -5,6 +5,8 @@ import qualified Data.ByteString.Char8 as C8
 import Data.ByteString.Char8 (ByteString)
 import Data.Conduit.Network
 
+import Data.Char (isDigit)
+
 import System.Random (getStdGen)
 import System.Environment (getArgs)
 import Control.Monad.IO.Class (liftIO)
@@ -23,7 +25,25 @@ conduit :: Conduit ByteString IO ByteString
 conduit = do
   g <- liftIO getStdGen
   CL.sourceList (BingoGame.chooseRandomNumbers g )
+  -- TODO: get Ints by promptInput before CL.map
   =$= CL.map ( withNL . C8.pack . show )
+  =$= promptInput
+
+promptInput :: Conduit ByteString IO ByteString
+promptInput = do
+  mbs <- await
+  liftIO $ putStr "Press Enter, input a number (1..75) or Q to quit: "
+  input <- liftIO getLine
+  case input of
+       ('Q':_) -> liftIO $ putStrLn "Bye."
+       _ -> do
+         if all isDigit input
+           then yield $ C8.pack input
+           else case mbs of
+                     Just bs -> yield bs
+                     Nothing -> do
+                       liftIO $ putStrLn "No number given!"
+                       promptInput
 
 withNL :: ByteString -> ByteString
 withNL s = C8.append s "\n"
