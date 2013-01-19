49c99a4d7c0bf2c7dab9eec25a5950aec13813b2
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2013-01-19 17:53:50 +0900

    written server and client. but fails to mark any cell. need test\!

--- binguit-client.hs
+++ binguit-client.hs
@@ -1,3 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Conduit
 import Data.Conduit.Network
-import BingoGame
+import qualified Data.Conduit.Binary as CB
+
+import qualified Data.ByteString.Char8 as C8
+import Data.ByteString.Char8 (ByteString)
+
+import Control.Concurrent (threadDelay)
+
+import System.Environment (getArgs)
+import System.Random (getStdGen)
+import Control.Monad.IO.Class
+
+import qualified BingoGame
+import BingoGame (BingoGame)
+
+main :: IO ()
+main = do
+  host:port:_ <- getArgs
+  g <- liftIO getStdGen
+  let bingo = BingoGame.newGame g
+  runTCPClient (ClientSettings (read port) host) (bingoClient bingo)
+
+bingoClient :: BingoGame -> Application IO
+bingoClient bingo src sink = do
+  putStrLnL "Your Initial Bingo Sheet:"
+  printL bingo
+  src $$ CB.lines =$= readAsInts =$= binguit bingo =$ sink
+
+binguit :: BingoGame -> Conduit Int IO ByteString
+binguit bingo = do
+  putStrLnL "Press Enter to get the next number, or enter 'Q' to quit"
+  command <- liftIO C8.getLine
+  if "Q" `C8.isPrefixOf` command
+    then return ()
+    else do
+      mn <- await
+      case mn of
+           Nothing ->
+             putStrLnL "Connection closed!"
+           Just n -> do
+             putStrLnL $ "Got " ++ show n ++ ". Can any cell marked...?"
+             liftIO $ threadDelay sec
+             let bingo' = BingoGame.markCell n bingo
+             printL bingo'
+             binguit bingo'
+  where
+    sec = ( 1000 )
+
+putStrLnL :: MonadIO m => String -> m ()
+putStrLnL = liftIO . putStrLn
+
+printL :: (MonadIO m, Show a) => a -> m ()
+printL = liftIO . print
+
+readAsInts :: Conduit ByteString IO Int
+readAsInts =
+  awaitForever $ yield . read . C8.unpack
--- binguit-server.hs
+++ binguit-server.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Conduit
+import qualified Data.Conduit.List as CL
+import qualified Data.ByteString.Char8 as C8
+import Data.ByteString.Char8 (ByteString)
 import Data.Conduit.Network
-import System.Random
-import BingoGame
 
+import System.Random (getStdGen)
+import System.Environment (getArgs)
+import Control.Monad.IO.Class (liftIO)
 
+import qualified BingoGame
+
+main :: IO ()
+main = do
+  port:_ <- getArgs
+  runTCPServer (ServerSettings (read port) HostAny) app
+
+app :: Application IO
+app src sink = src $$ conduit =$ sink
+
+conduit :: Conduit ByteString IO ByteString
+conduit = do
+  g <- liftIO getStdGen
+  CL.sourceList (BingoGame.chooseRandomNumbers g )
+  =$= CL.map ( withNL . C8.pack . show )
+
+withNL :: ByteString -> ByteString
+withNL s = C8.append s "\n"
